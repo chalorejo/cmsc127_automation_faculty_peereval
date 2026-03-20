4ae2d7ce593ada7590a209339a78fd6f601cc1f4
SenpaiCuber <jdbuhayan@up.edu.ph>
2026-03-20 22:30:42 +0800

    chore: backend boilerplate code
    - Boilerplate code for nest.js is done
    - connection with neon postgre database also done
    - .env file is in the drive (CMSC 127 files) sent by ate Gracie

---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports:[
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false, 
+          },
+        },
+      }),
+    }),
+  ],
+})
+export class AppModule {}
